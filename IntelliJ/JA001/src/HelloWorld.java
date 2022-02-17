ada91bd42766dc67cfc17737152815ebb8f4cc45
Sivatma Maharaj <sivatma.maharaj@gmail.com>
2022-02-17 12:04:51 -0500

    Just making sure github is working.

--- IntelliJ/JA001/src/HelloWorld.java
+++ IntelliJ/JA001/src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!  This is written in IntelliJ and stored in git hub");
+        System.out.println("This is another change just to see if this github thing is working");
     }
 }
