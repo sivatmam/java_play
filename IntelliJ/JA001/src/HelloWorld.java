a5a7e9ab297d86ea53d67cb1b50927527c976d78
Sivatma Maharaj <sivatma.maharaj@gmail.com>
2022-02-17 12:06:45 -0500

    Keep making commits to see if I can get github integration working.

--- IntelliJ/JA001/src/HelloWorld.java
+++ IntelliJ/JA001/src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!  This is written in IntelliJ and stored in git hub");
         System.out.println("This is another change just to see if this github thing is working");
+        System.out.println("A third change");
     }
 }
